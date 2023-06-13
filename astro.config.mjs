6b233e4f3ef04f86d3221c91d8b405feb30107af
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-06-13 19:38:32 +0200

    Removed unnecessary dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://your.domain.here",
-  integrations: [mdx(), sitemap(), tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp",
-    cacheDir: "./.cache/image",
-    logLevel: "debug"
-  }), react()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+      cacheDir: "./.cache/image",
+      logLevel: "debug",
+    }),
+  ],
 });
