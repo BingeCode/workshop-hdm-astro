7fb3544e4ccd75e20756395f0388726b6fe99e4f
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-06-20 20:43:46 +0200

    Init Übung 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
-import image from "@astrojs/image";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -11,6 +11,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  markdown: {
+    remarkPlugins: [remarkToc],
+  },
   site: "https://your.domain.here",
   integrations: [
     mdx(),
